e8112678f913104330c84f50be7d0215ccc8756d
Guido Guenther <agx@sigxcpu.org>
2007-11-22 17:48:35 +0100

    Imported Debian patch 0.5.0-0ubuntu0~ppa2

--- src/graphWidgets/pysparklinemodule.c
+++ src/graphWidgets/pysparklinemodule.c
@@ -4,7 +4,7 @@
 
 
 
-#line 3 "../../../src/graphWidgets/pysparklinemodule.override"
+#line 3 "./pysparklinemodule.override"
 #include <Python.h>               
 #include "pygobject.h"
 #include "sparkline.h"
