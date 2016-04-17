7f0c8b30d5c1d36193afb43192644dc6d9c7087b
Guido Günther <agx@sigxcpu.org>
2016-04-17 15:58:00 +0200

    autopkgtest: Run virt-manager as well

--- debian/tests/virt-manager.sh
+++ debian/tests/virt-manager.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-set -e
-
 # At least check we can execute the main binary
 # to catch missing python dependenies
-for p in install xml convert clone; do
-    virt-$p --help
-done
+
+set -e
+
+xvfb-run virt-manager --help
